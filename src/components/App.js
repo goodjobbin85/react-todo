43a52a6bec95d303717edfc23b9d802ff1fb3a0f
Thomas Dubiel <td1729@hotmail.com>
2021-05-04 13:16:45 -0500

    stub out basic App component

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react' 
+
+function App() { 
+    return ( 
+        <div> 
+            <input type="check"></input> 
+            <p>onions</p>
+
+            <input type="check"></input> 
+            <p>bacon</p>
+
+            <input type="check"></input> 
+            <p>lettuce</p>
+
+            <input type="check"></input> 
+            <p>tomatoes</p>
+        </div>
+    )
+} 
+
+export default App;
